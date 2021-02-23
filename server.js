57cfc79cddd06708d1ab76a5996b355cc8b4b3bb
Usman3278 <usman3278@gmail.com>
2021-02-23 13:14:48 -0500

    Added heroku port

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5000;
 
-var express = require("express");
+const express = require("express");
 
 
 
-var app = express();
+const app = express();
 
 
 app.use(express.static("public"));
@@ -24,8 +24,10 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-//Start server 
+//Listener
 app.listen(PORT, function() {
     //Log serve when server has started
     console.log("server listening on: http://localhost:" + PORT);
 });
+
+  
