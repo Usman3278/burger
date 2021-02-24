1c3a727ac3bbc7608fcae8bed2d2069644ab7673
Usman3278 <usman3278@gmail.com>
2021-02-24 09:33:33 -0500

    modied readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Eat-Da-Burger
 
 ## Description
-This app allows the user to add any burger to the connected MySQL database, consume that burger (change the devoured status), and then delete it from the database. 
+This app allows the user to add any burger to the connected MySQL database, consume that burger change the devoured status and then delete it from the database. 
 
 # Screenshots
 
--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
   } else {
-  var connection = mysql.createConnection({
+
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -14,12 +15,12 @@ if (process.env.JAWSDB_URL) {
     });
   };
   //Make connection.
-  connection.connect(function(err) {
+  connection.connect((err) => {
     if (err) {
-      console.error("error connecting: " + err.stack);
+      console.error(`error connecting: ${err.stack}`);
       return;
     }
-    console.log("connected as id " + connection.threadId);
+    console.log(`connected as id ${connection.threadId}`);
   });
 
 //Export connection
